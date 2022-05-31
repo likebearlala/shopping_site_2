e72d25eec869e6f27bc59d1970a5d490d1e04fce
likebearlala <lalabear202020@gmail.com>
2022-06-01 01:52:55 +0800

    0601 UPDATE

---
+++ assets/js/sign_In.js
@@ -0,0 +1,8 @@
+        function IfNull() {
+            if (user_email = null) {
+                alert("請輸入帳號!!不可為空值!!");
+            }
+            if (user_password = null) {
+                alert("請輸入密碼!!不可為空值!!");
+            }
+        }
