4a80ebbac59406c65d68ec3040b58b6803045d1e
likebearlala <lalabear202020@gmail.com>
2022-06-01 16:25:30 +0800

    0601 小修

--- assets/js/sign_In.js
+++ assets/js/sign_In.js
@@ -1,4 +1,6 @@
         function IfNull() {
+            var user_email=document.getElementById("user_email").value;
+            var user_password=document.getElementById("user_password").value;
             if (user_email = null) {
                 alert("請輸入帳號!!不可為空值!!");
             }
