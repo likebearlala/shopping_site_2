3cf1435a0fc52fed082d11c95b62367b23e45762
likebearlala <lalabear202020@gmail.com>
2022-06-05 23:25:37 +0800

    確認訂單有8成了

--- assets/js/success.js
+++ assets/js/success.js
@@ -1,20 +1,9 @@
 function success(){
     alert("結帳成功!");
     // location.replace("../../index.html");
-    window.location.href = '../../index.html';
+    window.location.href = '../index.html';
 }
 function goback(){
-    alert("結帳成功!");
-    // location.replace("../../index.html");
+    // location.replace("cart_checkout.html");
     window.location.href = 'cart_checkout.html';
 }
-// function goback(){
-//     window.onclick = function(event) {
-//         if (event.target == modal) {
-//           modal.style.display = "none";
-//            location.replace("index.html");
-//         }
-// }}
-// function Goindex() {
-//     window.location.href = 'index.html';
-// }
