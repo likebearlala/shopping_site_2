91f3e8c116c2aff40ed19bbccc9534ba5f4133c9
likebearlala <lalabear202020@gmail.com>
2022-06-07 23:19:25 +0800

    0607

--- assets/js/button_add_minus.js
+++ assets/js/button_add_minus.js
@@ -1,33 +1,33 @@
 
 
-// function minusNUM(num){
-// 	let input = Number(document.getElementsByClassName("number")[num].value);
-// 	if(input != 1){
-// 		document.getElementsByClassName('number')[num].value=input-1;
-// 	}
-	
-// }
-// function addNUM(num){
-// 	let input = Number(document.getElementsByClassName("number")[num].value);
-// 		document.getElementsByClassName('number')[num].value=input+1;
-// }
-function plus(num) {
-	var nex = num.previousElementSibling;
-	nex.value = Number(nex.value) + 1;
-	var s = parseInt(num.parentNode.previousElementSibling.innerHTML);
-	var number = s * Number(nex.value);
-	nex.parentNode.nextElementSibling.innerHTML = number;
-}
-
-function minus(num) {
-	var pre = num.nextElementSibling;
-	if (pre.value >= 1) {
-		pre.value = Number(pre.value) - 1;
-		var s = parseInt(num.parentNode.previousElementSibling.innerHTML);
-		var num = s * Number(pre.value);
-		pre.parentNode.nextElementSibling.innerHTML = num;
+function minusNUM(num){
+	let input = Number(document.getElementsByClassName("number")[num].value);
+	if(input != 1){
+		document.getElementsByClassName('number')[num].value=input-1;
 	}
+	
 }
+function addNUM(num){
+	let input = Number(document.getElementsByClassName("number")[num].value);
+		document.getElementsByClassName('number')[num].value=input+1;
+}
+// function plus(num) {
+// 	var nex = num.previousElementSibling;
+// 	nex.value = Number(nex.value) + 1;
+// 	var s = parseInt(num.parentNode.previousElementSibling.innerHTML);
+// 	var number = s * Number(nex.value);
+// 	nex.parentNode.nextElementSibling.innerHTML = number;
+// }
+
+// function minus(num) {
+// 	var pre = num.nextElementSibling;
+// 	if (pre.value >= 1) {
+// 		pre.value = Number(pre.value) - 1;
+// 		var s = parseInt(num.parentNode.previousElementSibling.innerHTML);
+// 		var num = s * Number(pre.value);
+// 		pre.parentNode.nextElementSibling.innerHTML = num;
+// 	}
+// }
 
 
 
