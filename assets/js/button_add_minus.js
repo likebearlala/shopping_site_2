97aad66b258cef0b977e1851a785d68689927e56
likebearlala <lalabear202020@gmail.com>
2022-06-09 04:24:08 +0800

    購物車自動計算-完成

--- assets/js/button_add_minus.js
+++ assets/js/button_add_minus.js
@@ -5,11 +5,32 @@ function minusNUM(num){
 	if(input != 1){
 		document.getElementsByClassName('number')[num].value=input-1;
 	}
-	
+	var td=document.getElementsByTagName("td");
+	let amount;
+	let total=0;
+	for(var i= 0;i<((td.length-3)/5);i++){
+	   let text=Number(td[1+(i*5)].innerHTML);
+	   let test=Number(document.getElementsByClassName("number")[i].value);
+	   amount=text*test;
+		td[3+(i*5)].innerHTML=amount;
+		total+=amount;
+	}
+	document.getElementById("total").innerHTML=total;
 }
 function addNUM(num){
 	let input = Number(document.getElementsByClassName("number")[num].value);
 		document.getElementsByClassName('number')[num].value=input+1;
+		var td=document.getElementsByTagName("td");
+		let amount;
+		let total=0;
+		for(var i= 0;i<((td.length-3)/5);i++){
+		   let text=Number(td[1+(i*5)].innerHTML);
+		   let test=Number(document.getElementsByClassName("number")[i].value);
+		   amount=text*test;
+			td[3+(i*5)].innerHTML=amount;
+			total+=amount;
+		}
+		document.getElementById("total").innerHTML=total;
 }
 // function plus(num) {
 // 	var nex = num.previousElementSibling;
